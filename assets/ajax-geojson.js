67c9c26884721d3509863876dd93c1e2eceee18f
yafred <fred599@laposte.net>
2016-11-17 21:13:58 +0100

    init

---
+++ assets/ajax-geojson.js
@@ -0,0 +1,62 @@
+processGeoJson( {
+	    "type": "FeatureCollection",
+	    "features": [
+	    {
+	      "type": "Feature",
+	      "properties": {
+	         "guid": "3504d836612b",
+	         "name": "Le Mont Saint-Michel",
+	         "url": "https://www.youtube.com/watch?v=1gXsWComxjo",
+	         "thumbnail": "https://i.ytimg.com/vi/1gXsWComxjo/hqdefault.jpg"
+	      },
+	      "geometry": {
+	        "type": "Point",
+	        "coordinates": [
+	        
+	          -1.511478,
+	          48.635971
+	        ]
+	      }
+	    }
+	    ,
+	  
+	    {
+	      "type": "Feature",
+	      "properties": {
+	         "guid": "74b71b32c51b",
+	         "name": "Le Cap Blanc Nez - Cote d'Opale",
+	         "url": "https://www.youtube.com/watch?v=KK9yW65gkUI",
+	         "thumbnail": "https://i.ytimg.com/vi/KK9yW65gkUI/hqdefault.jpg"
+	      },
+	      "geometry": {
+	        "type": "Point",
+	        "coordinates": [
+	        
+	          1.705176,
+	          50.924056
+	        ]
+	      }
+	    }
+	    ,
+	  
+	    {
+	      "type": "Feature",
+	      "properties": {
+	         "guid": "a9135e212af6",
+	         "name": "Antibes",
+	         "url": "https://www.youtube.com/watch?v=qxHQj6rrj_w",
+	         "thumbnail": "https://i.ytimg.com/vi/qxHQj6rrj_w/hqdefault.jpg"
+	      },
+	      "geometry": {
+	        "type": "Point",
+	        "coordinates": [
+	        
+	          7.121372,
+	          43.584991
+	        ]
+	      }
+	    }
+	    
+	  
+	  ]
+	});
